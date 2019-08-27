c9752208815b38b7dfea10ceb940c64606da05f9
Robert Alexander <roalexan@microsoft.com>
2019-08-27 13:55:17 -0400

    Add link showing how to disable selinux on CentOS

--- disable-selinux.sh
+++ disable-selinux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-linux
+# https://linuxize.com/post/how-to-disable-selinux-on-centos-7/
 sed -i "/^SELINUX=enforcing/c\SELINUX=disabled" /etc/selinux/config
 #sudo shutdown -r now
