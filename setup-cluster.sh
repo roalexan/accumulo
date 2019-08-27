2bc04dc551d51a56ac5560fb180ddb0ed8d35135
Robert Alexander <roalexan@microsoft.com>
2019-08-27 15:32:53 -0400

    Create setup-cluster.sh

---
+++ setup-cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install Python 3.6 and create a virtual environment
+
+yum install -y epel-release
+yum install -y python36 python36-devel python36-setuptools
+python36 /usr/lib/python3.6/site-packages/easy_install.py pip
+python3.6 -m venv env
+source ~/env/bin/activate
+
+# Install Ansible and VMSS (virtual machine scale set) patch
+
+yum check-update
+yum install -y gcc libffi-devel python-devel openssl-devel
+yum install -y python-pip python-wheel
+pip install ansible[azure]
+yum -y install git
+git clone https://github.com/ansible/ansible
+cp ansible/lib/ansible/modules/cloud/azure/azure_rm_virtualmachinescaleset.py env/lib/python3.6/site-packages/ansible/modules/cloud/azure/
