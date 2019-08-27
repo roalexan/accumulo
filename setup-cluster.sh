611961ab3121ae43f9dbdc6cf6cd1ca90d0d0387
Robert Alexander <roalexan@microsoft.com>
2019-08-27 17:06:56 -0400

    Check that python environment being created

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cd /root
-
 # Install Python 3.6 and create a virtual environment
 
-yum install -y epel-release
-yum install -y python36 python36-devel python36-setuptools
-python36 /usr/lib/python3.6/site-packages/easy_install.py pip
+sudo yum install -y epel-release
+sudo yum install -y python36 python36-devel python36-setuptools
+python36 --version
+sudo python36 /usr/lib/python3.6/site-packages/easy_install.py pip
 python3.6 -m venv env
 source ~/env/bin/activate
+python --version
 
 # Install Ansible and VMSS (virtual machine scale set) patch
 
-yum check-update
-yum install -y gcc libffi-devel python-devel openssl-devel
-yum install -y python-pip python-wheel
-pip install ansible[azure]
-yum -y install git
-git clone https://github.com/ansible/ansible
-cp ansible/lib/ansible/modules/cloud/azure/azure_rm_virtualmachinescaleset.py env/lib/python3.6/site-packages/ansible/modules/cloud/azure/
+#yum check-update
+#yum install -y gcc libffi-devel python-devel openssl-devel
+#yum install -y python-pip python-wheel
+#pip install ansible[azure]
+#yum -y install git
+#git clone https://github.com/ansible/ansible
+#cp ansible/lib/ansible/modules/cloud/azure/azure_rm_virtualmachinescaleset.py env/lib/python3.6/site-packages/ansible/modules/cloud/azure/
