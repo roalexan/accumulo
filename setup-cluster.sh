60757b8158f73173c333d17ff90753e2ac548212
Robert Alexander <roalexan@microsoft.com>
2019-08-28 14:32:17 -0400

    try to create python environment

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-cd /root
-
 # Install Python 3.6 and create a virtual environment
+
+cd ~
 
 sudo yum install -y epel-release
 sudo yum install -y python36 python36-devel python36-setuptools
-python36 --version
 sudo python36 /usr/lib/python3.6/site-packages/easy_install.py pip
 python3.6 -m venv env
-source ~/env/bin/activate
-python --version
 
 # Install Ansible and VMSS (virtual machine scale set) patch
 
