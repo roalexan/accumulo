632af33ec0a4b502f95dae2278b926b3ed377dea
Robert Alexander <roalexan@microsoft.com>
2019-08-28 14:50:41 -0400

    install ansible 2

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -7,7 +7,7 @@ cd ~
 sudo yum install -y epel-release
 sudo yum install -y python36 python36-devel python36-setuptools
 sudo python36 /usr/lib/python3.6/site-packages/easy_install.py pip
-python3.6 -m venv env
+python3.6 -m venv ~/env
 source ~/env/bin/activate
 
 # Install Ansible and VMSS (virtual machine scale set) patch
@@ -19,3 +19,11 @@ pip install ansible[azure]
 sudo yum -y install git
 git clone https://github.com/ansible/ansible
 cp ansible/lib/ansible/modules/cloud/azure/azure_rm_virtualmachinescaleset.py env/lib/python3.6/site-packages/ansible/modules/cloud/azure/
+
+#yum check-update
+#yum install -y gcc libffi-devel python-devel openssl-devel
+#yum install -y python-pip python-wheel
+#pip install ansible[azure]
+#yum -y install git
+#git clone https://github.com/ansible/ansible
+#cp ansible/lib/ansible/modules/cloud/azure/azure_rm_virtualmachinescaleset.py env/lib/python3.6/site-packages/ansible/modules/cloud/azure/
