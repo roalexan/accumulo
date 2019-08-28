60bc37c257d6e1d56178952a6f6cfd32ba939f0e
Robert Alexander <roalexan@microsoft.com>
2019-08-28 15:39:49 -0400

    start agent and install azure cli

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -20,10 +20,23 @@ sudo yum -y install git
 git clone https://github.com/ansible/ansible
 cp ansible/lib/ansible/modules/cloud/azure/azure_rm_virtualmachinescaleset.py env/lib/python3.6/site-packages/ansible/modules/cloud/azure/
 
-#yum check-update
-#yum install -y gcc libffi-devel python-devel openssl-devel
-#yum install -y python-pip python-wheel
-#pip install ansible[azure]
-#yum -y install git
-#git clone https://github.com/ansible/ansible
-#cp ansible/lib/ansible/modules/cloud/azure/azure_rm_virtualmachinescaleset.py env/lib/python3.6/site-packages/ansible/modules/cloud/azure/
+# Download the Magna Carta repo zip
+
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
+mkdir ~/fluo-muchos
+wget https://roalexan.blob.core.windows.net/accumulo/fluo-muchos.zip --output-document ~/fluo-muchos/fluo-muchos.zip
+unzip ~/fluo-muchos/fluo-muchos.zip -d ~/fluo-muchos
+
+# Setup agent forwarding
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+export ANSIBLE_LOG_PATH=~/play.log
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+
+# Install Azure CLI
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+sudo yum install -y azure-cli
+sed -i '/^PYTHONPATH=/c\PYTHONPATH=/usr/lib64/az/lib/python2.7/site-packages python2 -sm azure.cli "$@"' $(which az)
