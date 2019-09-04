bdd3f4a4818e492901eaa9e08019ad15bb5eba10
Robert Alexander <roalexan@microsoft.com>
2019-09-04 19:56:53 -0400

    activate python environment

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -56,7 +56,7 @@ echo "Install Python 3.6 and create a virtual environment"
 #sudo yum install -y python36 python36-devel python36-setuptools
 #sudo python36 /usr/lib/python3.6/site-packages/easy_install.py pip
 #python3.6 -m venv ~/env
-#source ~/env/bin/activate
+source ~/env/bin/activate
 
 echo "Install Ansible and VMSS (virtual machine scale set) patch"
 #sudo yum check-update
