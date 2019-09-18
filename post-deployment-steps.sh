9b2cafac2f9006961060c5ecc7d0403c7b614af9
Robert Alexander <roalexan@microsoft.com>
2019-09-18 14:40:16 -0400

    update echo comments

--- post-deployment-steps.sh
+++ post-deployment-steps.sh
@@ -42,10 +42,11 @@ while read hostname ipaddress
 do
 	# https://stackoverflow.com/questions/6351022/executing-ssh-command-in-a-bash-shell-script-within-a-loop
 	# https://superuser.com/questions/125324/how-can-i-avoid-sshs-host-verification-for-known-hosts
+	echo "set properties in "$hostname
 	ssh -T -o "StrictHostKeyChecking no" $adminUsername@$hostname << 'EOF'
-	echo "set yarn.nodemanager.resource.memory-mb in "$hostname
+	echo "set yarn.nodemanager.resource.memory-mb"
 	sed -i '/yarn.nodemanager.resource.memory-mb/{n; s/<value>.*<\/value>/<value>32768<\/value>/}' ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml
-	echo "set yarn.scheduler.maximum-allocation-mb in "$hostname
+	echo "set yarn.scheduler.maximum-allocation-mb"
 	if ! grep -q yarn.scheduler.maximum-allocation-mb ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml; then
 		lineIndex=`sed -n '/yarn.nodemanager.resource.memory-mb/=' ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml`
 		lineIndex=$(($lineIndex + 2))
@@ -53,11 +54,11 @@ do
 		elem=$(echo $elem | sed 's/\//\\\//g')
 		sed -i "${lineIndex}a\\\t${elem}" ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml
 	fi
-	echo "set spark.yarn.am.cores in "$hostname
+	echo "set spark.yarn.am.cores"
 	if ! grep -q spark.yarn.am.cores ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf; then
 		echo "spark.yarn.am.cores                4" >> ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf 
 	fi
-	echo "set spark.yarn.am.memory in "$hostname
+	echo "set spark.yarn.am.memory"
 	if ! grep -q spark.yarn.am.memory ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf; then
 		echo "spark.yarn.am.memory               12g" >> ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf 
 	fi
