6683fd6ac63c0acfd330b3cd866d3c51bbbf7f8b
Robert Alexander <roalexan@microsoft.com>
2019-09-18 14:34:13 -0400

    use heredoc for all remote commands

--- post-deployment-steps.sh
+++ post-deployment-steps.sh
@@ -40,13 +40,12 @@ fi
 # https://stackoverflow.com/questions/7619438/bash-read-a-file-line-by-line-and-process-each-segment-as-parameters-to-other-p#7619467
 while read hostname ipaddress
 do
-	echo "set yarn.nodemanager.resource.memory-mb in "$hostname
 	# https://stackoverflow.com/questions/6351022/executing-ssh-command-in-a-bash-shell-script-within-a-loop
 	# https://superuser.com/questions/125324/how-can-i-avoid-sshs-host-verification-for-known-hosts
-	ssh -n -o "StrictHostKeyChecking no" $adminUsername@$hostname "sed -i '/yarn.nodemanager.resource.memory-mb/{n; s/<value>.*<\/value>/<value>32768<\/value>/}' ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml"
-	
-	echo "set yarn.scheduler.maximum-allocation-mb in "$hostname
 	ssh -T -o "StrictHostKeyChecking no" $adminUsername@$hostname << 'EOF'
+	echo "set yarn.nodemanager.resource.memory-mb in "$hostname
+	sed -i '/yarn.nodemanager.resource.memory-mb/{n; s/<value>.*<\/value>/<value>32768<\/value>/}' ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml
+	echo "set yarn.scheduler.maximum-allocation-mb in "$hostname
 	if ! grep -q yarn.scheduler.maximum-allocation-mb ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml; then
 		lineIndex=`sed -n '/yarn.nodemanager.resource.memory-mb/=' ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml`
 		lineIndex=$(($lineIndex + 2))
@@ -54,9 +53,11 @@ do
 		elem=$(echo $elem | sed 's/\//\\\//g')
 		sed -i "${lineIndex}a\\\t${elem}" ~/install/hadoop-3.2.0/etc/hadoop/yarn-site.xml
 	fi
+	echo "set spark.yarn.am.cores in "$hostname
 	if ! grep -q spark.yarn.am.cores ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf; then
 		echo "spark.yarn.am.cores                4" >> ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf 
 	fi
+	echo "set spark.yarn.am.memory in "$hostname
 	if ! grep -q spark.yarn.am.memory ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf; then
 		echo "spark.yarn.am.memory               12g" >> ~/install/spark-2.4.3-bin-without-hadoop/conf/spark-defaults.conf 
 	fi
