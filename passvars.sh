168c86e72ae732a6c724a4be8ffe74ff524962a3
Robert Alexander <roalexan@microsoft.com>
2019-10-16 12:43:35 -0400

    Update passvars.sh

--- passvars.sh
+++ passvars.sh
@@ -3,6 +3,29 @@ rm --force ./passvars.log
 exec > >(tee --append ./passvars.log)
 exec 2>&1
 
+date
+echo "Read the options"
+TEMP=`getopt -o d --long data-size: -- "$@"`
+eval set -- "$TEMP"
+
+echo "Extract options and their arguments into variables"
+while true ; do
+    case "$1" in
+        -d|--data-size)
+            dataSize=$2 ; shift 2;;
+        --) shift ; break ;;
+        *) echo "ERROR: Unable to get variables from arguments" ; exit 1 ;;
+    esac
+done
+
+if [ -z "$dataSize" ]
+then
+    echo "Missing required argument: -d | data-size"
+    exit 1
+fi
+
+echo "dataSize 1: ${dataSize}"
+
 adminusername="rba1"
 hostname="4nodecluster-0"
 ssh -T -o "StrictHostKeyChecking no" ${adminusername}@${hostname} << EOF
