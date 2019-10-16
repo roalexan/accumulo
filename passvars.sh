ee9cd278f477f9b9c6a76fcace065978acacba66
Robert Alexander <roalexan@microsoft.com>
2019-10-16 12:40:28 -0400

    Update passvars.sh

--- passvars.sh
+++ passvars.sh
@@ -1,10 +1,14 @@
-dataSize="1G"
+cd ~
+rm --force ./passvars.log
+exec > >(tee --append ./passvars.log)
+exec 2>&1
+
 adminusername="rba1"
 hostname="4nodecluster-0"
 ssh -T -o "StrictHostKeyChecking no" ${adminusername}@${hostname} << EOF
 adminUsername=$(whoami)
-echo "dataSize: ${dataSize}"
 echo "adminUsername: ${adminUsername}"
+echo "dataSize: ${dataSize}"
 JAR="file:////home/${adminUsername}/webscale-ai-test/target/accumulo-spark-shaded.jar"
 echo "JAR: ${JAR}"
 EOF
